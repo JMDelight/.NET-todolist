36069254ad27c32a88297f0f95ea18452c9c9989
Ben Logue and Joel Delight <ben+joel@>
2016-09-19 09:18:26 -0700

    update project to core, flesh out categories controller.

---
+++ src/ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoList.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = db.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Details(int id)
+        {
+            Category selectedCategory = db.Categories.FirstOrDefault(c => c.CategoryId == id);
+            List<Item> items = db.Items.Where(i => i.CategoryId == id).ToList();
+            ViewBag.Category = selectedCategory;
+            return View(items);
+        }
+        public IActionResult Check(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            thisItem.Done = !thisItem.Done;
+            db.SaveChanges();
+            return RedirectToAction("Details", new { id = thisItem.CategoryId } ) ;
+        }
+    }
+}
--- src/ToDoList/Models/ToDoListContext.cs
+++ src/ToDoList/Models/ToDoListContext.cs
@@ -12,6 +12,15 @@ namespace ToDoList.Models
         public virtual DbSet<Category> Categories { get; set; }
         public virtual DbSet<Item> Items { get; set; }
 
+        public ToDoListContext(DbContextOptions<ToDoListContext> options)
+            : base(options)
+        {
+        }
+        public ToDoListContext() { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ToDoList;integrated security=True");
